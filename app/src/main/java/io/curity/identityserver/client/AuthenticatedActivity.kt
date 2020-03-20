354558bc1341008906f3f622b3f4b66fcd71a70f
Daniel Lindau <daniel@curity.io>
2020-03-20 15:59:59 +0100

    Initial commit

---
+++ app/src/main/java/io/curity/identityserver/client/AuthenticatedActivity.kt
@@ -0,0 +1,67 @@
+/*
+ *  Copyright 2020 Curity AB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package io.curity.identityserver.client
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.content.ContentValues.TAG
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import org.json.JSONObject
+
+class AuthenticatedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authenticated)
+
+
+        if (intent.extras == null) return showError("No data in intent")
+        if (intent.extras?.get("id_token_claims") == null) {
+            return showError("Do not find id token claims")
+        }
+
+        val jsonString = intent.extras?.get("id_token_claims") as String
+        val jsonObject = JSONObject(jsonString)
+        val subject = jsonObject["sub"]
+        val authTime = jsonObject["auth_time"]
+        val acr = jsonObject["acr"]
+        val title = findViewById<TextView>(R.id.hello_subject)
+        title.text = getString(R.string.hello_subject, subject)
+
+        val authnDescription = findViewById<TextView>(R.id.authn_description)
+        authnDescription.text = getString(R.string.authn_description, authTime, acr)
+
+        val logoutButton = findViewById<Button>(R.id.logout)
+        logoutButton.setOnClickListener(logout())
+
+    }
+
+    private fun logout(): (View) -> Unit = {
+        val logoutIntent = Intent(applicationContext, MainActivity::class.java)
+        startActivity(logoutIntent)
+    }
+
+    private fun showError(error: String) {
+        val authnDescription = findViewById<TextView>(R.id.authn_description)
+        authnDescription.text = error
+        Log.w(TAG, error)
+    }
+}
---
+++ app/src/main/java/io/curity/identityserver/client/MainActivity.kt
@@ -0,0 +1,147 @@
+/*
+ *  Copyright 2020 Curity AB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package io.curity.identityserver.client
+
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.annotation.MainThread
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import net.openid.appauth.*
+import org.jose4j.jwt.consumer.JwtConsumerBuilder
+
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val REQUEST_CODE_AUTHORIZATION_INTENT = 100
+    }
+
+    private lateinit var serviceConfiguration: AuthorizationServiceConfiguration
+    private lateinit var authorizationService: AuthorizationService
+    private lateinit var authState: AuthState
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+
+        findViewById<Button>(R.id.loginButton).setOnClickListener {
+            val request = buildAuthorizationRequest()
+            val authIntent = authorizationService.getAuthorizationRequestIntent(request)
+            startActivityForResult(authIntent, REQUEST_CODE_AUTHORIZATION_INTENT)
+        }
+
+        AuthorizationServiceConfiguration.fetchFromIssuer(
+            Uri.parse("https://dlindau.ngrok.io/~")) { config, ex ->
+            handleConfigurationRetrievalResult(config, ex)
+        }
+
+        authorizationService = AuthorizationService(this)
+    }
+
+    @MainThread
+    private fun handleConfigurationRetrievalResult(config: AuthorizationServiceConfiguration?,
+                                                   ex: AuthorizationException?
+    ) {
+        if (config == null) {
+            Log.i(TAG, "Failed to retrieve discovery document", ex)
+            return
+        }
+
+        Log.i(TAG, "Discovery document retrieved")
+        Log.d(TAG, config.toJsonString())
+        serviceConfiguration = config
+        authState = AuthState(serviceConfiguration)
+    }
+
+    private fun buildAuthorizationRequest(): AuthorizationRequest {
+        val clientId = "app-auth"
+        val redirectUri = Uri.parse("io.curity.client:/callback")
+        return AuthorizationRequest.Builder(serviceConfiguration, clientId, "code", redirectUri)
+            .setScopes("openid profile")
+            .setPrompt(AuthorizationRequest.Prompt.LOGIN)
+            .build()
+    }
+
+    override fun onActivityResult(requestCode: Int,
+                                  resultCode: Int,
+                                  data: Intent?) {
+        if (requestCode == REQUEST_CODE_AUTHORIZATION_INTENT && data != null) {
+            handleAuthorizationResponse(data)
+        }
+    }
+
+
+    private fun handleAuthorizationResponse(intent: Intent) {
+        val response = AuthorizationResponse.fromIntent(intent)
+        val error = AuthorizationException.fromIntent(intent)
+        when {
+            response != null -> {
+                Log.i(TAG, "Got an authorization response")
+            }
+            error != null -> {
+                Log.e(TAG, "Got an error from authorization request: {}", error)
+                throw error
+            }
+            else -> {
+                Log.e(TAG, "Got neither response or error in authorization callback")
+                throw RuntimeException("Got no response in authorization callback")
+            }
+        }
+        authorizationService.performTokenRequest(response.createTokenExchangeRequest(),
+            handleTokenResponse())
+    }
+
+    private fun handleTokenResponse(): (TokenResponse?, AuthorizationException?) -> Unit {
+        return { tokenResponse, exception ->
+            when {
+                tokenResponse != null -> {
+                    Log.i(TAG, "Got a token response: ${tokenResponse.idToken}")
+                    authState.update(tokenResponse, null)
+                    viewDataFromIdToken()
+                }
+                exception != null -> { // authorization failed, check ex for more details
+                    Log.e(TAG, "Got error response")
+                    throw exception
+                }
+                else -> {
+                    throw RuntimeException("Could not parse data in token response")
+                }
+            }
+        }
+    }
+
+    private fun viewDataFromIdToken() {
+        val jwtConsumer = JwtConsumerBuilder()
+            .setSkipSignatureVerification() // Not required in code flow, since the token is fetched from the server using TLS
+            .setRequireSubject()
+            .setAllowedClockSkewInSeconds(30)
+            .setExpectedIssuer("https://dlindau.ngrok.io/~")
+            .setExpectedAudience("app-auth")
+            .build()
+
+        val jwtClaims = jwtConsumer.processToClaims(authState.idToken)
+        val switchToAuthenticated = Intent(applicationContext, AuthenticatedActivity::class.java)
+        switchToAuthenticated.putExtra("id_token_claims", jwtClaims.toJson())
+        startActivity(switchToAuthenticated)
+    }
+}
