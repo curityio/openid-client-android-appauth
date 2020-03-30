6e4c0f0f0a374c903e158edeb0ed90274952f108
Daniel Lindau <daniel@curity.io>
2020-03-30 23:29:03 +0200

    copyright

--- app/src/main/java/io/curity/identityserver/client/WaitingActivity.kt
+++ app/src/main/java/io/curity/identityserver/client/WaitingActivity.kt
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2020. Curity AB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package io.curity.identityserver.client
 
 import android.content.ContentValues.TAG
@@ -29,7 +45,8 @@ class WaitingActivity : AppCompatActivity() {
             response != null -> {
                 Log.i(TAG, "Got an authorization response")
                 val tokenRequest = response.createTokenExchangeRequest(
-                    mapOf("client_secret" to ApplicationStateManager.registrationResponse.clientSecret))
+                    mapOf(
+                        "client_secret" to ApplicationStateManager.registrationResponse.clientSecret))
                 authorizationService.performTokenRequest(tokenRequest, handleTokenResponse())
             }
             else -> {
